d07905e509f9a186b34d5436cd6ebe5ae32aab82
jbowe <jbowe@webstaurantstore.com>
2024-01-16 14:11:30 -0500

    Refactor

--- go-with-tests/iteration/main.go
+++ go-with-tests/iteration/main.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repetitions int = 5
+
 func Repeat(rep string) string {
     var repeated string
-    for i := 0; i < 5; i++ {
-        repeated = repeated + rep
+    for i := 0; i < repetitions; i++ {
+        repeated += rep
     }
     return repeated
 }
